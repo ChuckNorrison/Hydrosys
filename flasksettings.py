77d2c1825bfab3eb58c652fb48cc4244432c50b6
ChuckNorrison <2964146+ChuckNorrison@users.noreply.github.com>
2024-10-09 17:44:04 +0200

    Security fix for flask
    dynamically create the secret key

--- flasksettings.py
+++ flasksettings.py
@@ -1,4 +1,7 @@
+import secrets
+
 # configuration
-SECRET_KEY = 'thisisnotsafe'
+#SECRET_KEY = 'thisisnotsafe'
+SECRET_KEY = secrets.token_hex(16)
 UPLOAD_FOLDER = "static/upload"
 MAX_CONTENT_LENGTH = 1048576
